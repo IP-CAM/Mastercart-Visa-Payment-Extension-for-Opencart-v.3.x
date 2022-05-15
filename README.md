c09dcfef275dbdee4aa4338038b5f73407fd8972
zhc <zhc4454991@163.com>
2022-05-15 15:51:43 +0800

    初始化

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # opencart-inst-exten2
+
+upload文件夹内为源码
+
+将upload文件夹压缩，重命名为 **.ocmod.zip
+
+在opencart后台进行安装即可使用
---
+++ admin/language/en-gb/extension/payment/inst_visa.php
@@ -0,0 +1,31 @@
+<?php
+
+// Heading 插件名称
+$_['heading_title']         = 'Inst Payment VISA';
+
+// Text
+$_['text_extension']        = 'extension';
+$_['text_success']          = 'success：Inst Payment VISA config has changed！';
+$_['text_edit']             = 'edit';
+$_['text_inst_visa']        = '<a target="_BLANK" href="https://www.inst.money/"><img src="view/image/payment/inst_visa.png" alt="Inst Pay VISA" title="Inst Pay VISA" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_enabled_webhooks'] = 'enabled (url : https://{host}?route=extension/payment/inst_visa/callback)';
+
+
+// Entry
+$_['entry_host']            = 'Domain:';
+$_['entry_api_key']         = 'API Key:';
+$_['entry_api_secret']      = 'API Secret:';
+$_['entry_api_passphrase']  = 'API Password:';
+$_['entry_webhooks_status'] = 'Webhooks of order status:';
+$_['entry_geo_zone']        = 'Geo Zone:';
+$_['entry_sort_order']      = 'Sort Order:';
+$_['entry_status']          = 'Status:';
+
+// Error
+$_['error_host']            = 'Please Input Domain!';
+$_['error_api_key']         = 'Please Input API Key!';
+$_['error_api_secret']      = 'Please Input API Secret!';
+$_['error_api_passphrase']  = 'Please Input API Password!';
+$_['error_permission']      = 'Error：No Permission of Inst Payment VISA!';
+
+?>
